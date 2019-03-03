f1d1e0f96638f24964e0d54d6a43ac6de2feca5f
Mr1173 <cmsyh29@163.com>
2019-03-03 14:16:06 +0800

    first try PatB1001

---
+++ src/Third/PatB1001.java
@@ -0,0 +1,24 @@
+package Third;
+
+import java.util.Scanner;
+
+/**
+ * @Author: hui
+ * @Date: 2019/3/3 13:52
+ */
+public class PatB1001 {
+    public static void main(String[] args) {
+        int step = 0;
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        while (n != 1) {
+            if (n % 2 == 0)
+                n = n / 2;
+            else
+                n = (3 * n + 1) / 2;
+            step++;
+        }
+        System.out.printf("%d\n",step);
+    }
+
+}
